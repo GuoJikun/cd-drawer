a1f7457c8ed3cf4af2e0653805d54b814c8f9ac5
GuoJikun <guojikun486546@163.com>
2023-08-22 14:19:33 +0800

    ✨ feat: 完善组件基础功能

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,56 +6,51 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface CeDrawer {
+        "close": () => Promise<void>;
+        "header": string;
+        "maskClosable": boolean;
+        "open": () => Promise<void>;
+        "placement": string;
+        "showHeader": boolean;
+        "visible": Boolean;
+        "width": string;
     }
 }
+export interface CeDrawerCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLCeDrawerElement;
+}
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLCeDrawerElement extends Components.CeDrawer, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLCeDrawerElement: {
+        prototype: HTMLCeDrawerElement;
+        new (): HTMLCeDrawerElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "ce-drawer": HTMLCeDrawerElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface CeDrawer {
+        "header"?: string;
+        "maskClosable"?: boolean;
+        "onClosed"?: (event: CeDrawerCustomEvent<any>) => void;
+        "placement"?: string;
+        "showHeader"?: boolean;
+        "visible"?: Boolean;
+        "width"?: string;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "ce-drawer": CeDrawer;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "ce-drawer": LocalJSX.CeDrawer & JSXBase.HTMLAttributes<HTMLCeDrawerElement>;
         }
     }
 }
--- src/components/ce-drawer/ce-drawer.tsx
+++ src/components/ce-drawer/ce-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Event, EventEmitter, Host, Method, Prop, Watch, h } from '@stencil/core';
 
 @Component({
   tag: 'ce-drawer',
@@ -6,13 +6,99 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class CeDrawer {
+  @Prop({
+    attribute: 'show',
+    mutable: true,
+    reflect: true,
+  })
+  visible: Boolean = false;
+
+  @Prop()
+  width: string = '36%';
+
+  @Prop({
+    attribute: 'show-header',
+    mutable: true,
+    reflect: true,
+  })
+  showHeader: boolean = false;
+
+  @Prop({
+    attribute: 'header',
+  })
+  header: string = '';
+
+  @Prop({
+    attribute: 'mask-closable',
+    mutable: true,
+    reflect: true,
+  })
+  maskClosable: boolean = true;
+
+  @Prop({
+    attribute: 'placement',
+    mutable: true,
+    reflect: true,
+  })
+  placement: string = 'right';
+
+  /**监听传入的 placement 是否符合要求*/
+  @Watch('placement')
+  validateName(val: string) {
+    const flag = ['left', 'right', 'top', 'bottom'].includes(val);
+    if (!flag) {
+      throw new Error('placement 必须是 left/right/top/bottom 其中之一');
+    }
+  }
+
+  renderHeader() {
+    if (this.showHeader) {
+      return (
+        <div class="ivy-drawer-header">
+          <slot name="header">{this.header}</slot>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 
   render() {
     return (
-      <Host>
-        <slot></slot>
+      <Host show={this.visible}>
+        <div class="ivy-mask" onClick={this.maskClose.bind(this)}></div>
+        <div
+          class="ivy-drawer"
+          style={{ width: ['left', 'right'].includes(this.placement) ? this.width : '100%', height: ['top', 'bottom'].includes(this.placement) ? this.width : '100%' }}
+        >
+          {this.renderHeader()}
+
+          <div class="ivy-drawer-body">
+            <slot></slot>
+          </div>
+        </div>
       </Host>
     );
   }
 
+  @Event() closed: EventEmitter;
+  closeHandler() {
+    this.closed.emit();
+  }
+
+  @Method()
+  async open() {
+    this.visible = true;
+  }
+  @Method()
+  async close() {
+    this.closeHandler();
+    this.visible = false;
+  }
+
+  maskClose() {
+    if (this.maskClosable) {
+      this.visible = false;
+    }
+  }
 }
